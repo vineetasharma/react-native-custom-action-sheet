122158440120167a2019aa29067c89b36834429c
Vineeta Sharma <vineetasharma18791@gmail.com>
2017-01-01 22:59:12 +0530

    removed the style from index file

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var ActionModal = React.createClass({
           <View style={styles.modalContainer}>
             <TouchableOpacity style={styles.container} onPress={this.props.onCancel}></TouchableOpacity>
             {this.props.children}
-            <Button style={styles.cancelText} onPress={this.props.onCancel} text={this.props.buttonText || "Cancel"} />
+            <Button onPress={this.props.onCancel} text={this.props.buttonText || "Cancel"} />
           </View>
         </Modal>
       </FadeInView>
@@ -35,9 +35,6 @@ var styles = StyleSheet.create({
     padding: 8,
     paddingBottom: 0,
     justifyContent: "flex-end"
-  },
-  cancelText: {
-    color: '#2e8995',
   },
 });
 
